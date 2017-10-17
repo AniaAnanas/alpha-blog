cb5de4a6c2701702483c7ab967e3c78cbfe7feaf
Ania <ania.chochowska@gmail.com>
2017-10-17 20:53:14 +0200

    Add restrictions to controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+	before_action :set_user, only: [:show, :edit, :update]
+	before_action :require_same_user, only: [:edit, :update]
+
+	
+
 	def index
 		@users = User.paginate(page: params[:page], per_page: 5)
 	end
@@ -9,7 +14,6 @@ class UsersController < ApplicationController
   end
 
   def show
-  	@user = User.find(params[:id])
   	@user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -25,11 +29,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-  	@user = User.find(params[:id])
+  	
   end
 
   def update
-  	@user = User.find(params[:id])
   	if @user.update(user_params)
   		flash[:succes] = "Your account was succesfully updated."
       redirect_to articles_path
@@ -42,6 +45,16 @@ class UsersController < ApplicationController
   def user_params
   	params.require(:user).permit(:username, :email, :password)
   end
+  def set_user
+		@user = User.find(params[:id])
+	end
+
+	def require_same_user
+		if current_user != @user
+			flash[:danger] = "You can only edit your own profile."
+			redirect_to root_path
+		end
+	end
 
  
 end
