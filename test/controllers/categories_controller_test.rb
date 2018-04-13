5637986be3a50d61ecd35899980da46dec3aa0b3
Ania <ania.chochowska@gmail.com>
2018-04-13 12:25:36 +0200

    Fix tests. Add Categories to nav bar

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -5,27 +5,27 @@ class CategoriesControllerTest < ActionController::TestCase
 
 	def setup
 		@category = Category.create(name: "sports")
-		@user = User.create(username: "john", email: "john.example.com", password: "password", admin: false)
+		@user = User.create(username: "john", email: "john@example.com", password: "password", admin: true )
 	end
 
 	test "should get categories index" do
 		get :index
 		assert_response :success
 	end
-
+=begin
 	test "should get new" do
 		session[:user_id] = @user.id
 		get :new
 		assert_response :redirect
 	end
+=end
 
-=begin
 	test "should get new" do
 		session[:user_id] = @user.id
 		get :new
 		assert_response :success
 	end
-=end
+
 
 
 	test "should get show" do
